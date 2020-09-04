91e7a963ae7e386eb22cea114bd0a41d82ced78a
Emmanuel Laborin <laborin@gmail.com>
2020-09-03 23:46:45 -0700

    add missing comma

--- SimpleOptionPages.php
+++ SimpleOptionPages.php
@@ -10,7 +10,7 @@
  * @version		1.0.0
  */
 
-namespace Laborin\SimpleOptionPages
+namespace Laborin\SimpleOptionPages;
 
 class SimpleOptionPages {
 	/* ==========================================================================
